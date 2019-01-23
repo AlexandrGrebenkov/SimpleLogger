7a30fadf5d386ab1c68fdab336d5912115ddf9b7
Alexandr <alexandrgrebenkov@gmail.com>
2019-01-23 14:23:27 +0500

    Cache

--- SimpleLogger/Target.cs
+++ SimpleLogger/Target.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 
 namespace SimpleLogger
 {
@@ -7,10 +8,64 @@ namespace SimpleLogger
     {
         /// <summary>Порог записи. В лог пишутся записи равные и выше заданного уровня</summary>
         public LogLevel LogLevel;
+
         /// <summary>
         /// Метод записи. <para/>
         /// Пример: write = message => Console.WriteLine(message);
         /// </summary>
-        public Action<object> write;
+        public Action<object> Write;
+
+        /// <summary>Настройки записи</summary>
+        public TargetSettings TargetSettings;
+
+        public Target(LogLevel logLevel, Action<object> write, TargetSettings targetSettings = null)
+        {
+            LogLevel = logLevel;
+            Write = write;
+            TargetSettings = targetSettings ?? new TargetSettings();
+        }
+
+        ConcurrentQueue<object> cache = new ConcurrentQueue<object>();
+
+        void AddToCache(object obj)
+        {
+            if (!TargetSettings.EnableCache) return;
+
+            cache.Enqueue(obj);
+            if (cache.Count > TargetSettings.CacheSize)
+            {
+                bool result;
+                do
+                {
+                    result = cache.TryDequeue(out object old);
+                } while (!result);
+            }
+        }
+
+        void WriteCache()
+        {
+            bool result;
+            while (cache.Count != 0)
+            {
+                do
+                {
+                    result = cache.TryDequeue(out object old);
+                } while (!result);
+                Write(result);
+            }
+        }
+
+        public void AddMessage(object obj, LogLevel level)
+        {
+            var message = $"{DateTime.Now}: {level} - {obj}";
+
+            if (TargetSettings.EnableCache && level >= TargetSettings.WriteCacheLevel) // Порог отображения записей кэша           
+                WriteCache();
+
+            if (level >= LogLevel)
+                Write(message);
+            else
+                AddToCache(message);
+        }
     }
 }
